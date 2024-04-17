8027a110e71badb111b5fc5de09913810222307b
Varun Garade <varungarade1151@gmail.com>
2024-04-17 15:16:47 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Navbar from "./Layout/Navbar";
 
 function App() {
   return (
-  <>
-    <h1 className='text-center font-extrabold'>Hello Infobell </h1>
-   </>
+    <>
+      <BrowserRouter>
+      <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
